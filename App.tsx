2ee80c0657a3112d9553f1488d066ceb1fb8747e
Ap274 <aldopiresaps@gmail.com>
2024-04-26 15:26:57 -0300

    feat: update Loading component and basic structure with fontsLoaded

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { config } from '@gluestack-ui/config';
+import { config } from './config/gluestack-ui.config';
 import { GluestackUIProvider, Text, View } from '@gluestack-ui/themed';
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
@@ -15,7 +15,7 @@ export default function App() {
         translucent
       />
 
-      <Loading />
+     { fontsLoaded ? <View/> : <Loading />}
     </GluestackUIProvider>
   );
 }
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,8 +2,8 @@ import { Spinner, Center } from '@gluestack-ui/themed';
 
 export function Loading() {
     return (
-        <Center flex={1}>
-            <Spinner size="large"/>
+        <Center flex={1} bg='$mygray700'> 
+            <Spinner size="large" color="$mygreen500"/>
         </Center>
     );
 }
