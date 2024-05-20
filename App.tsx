a92e3caef64b2444cbd9c9c9facc565859058e09
Ap274 <aldopiresaps@gmail.com>
2024-05-20 10:23:51 -0300

    feat: create SignUp screen

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { config } from './config/gluestack-ui.config';
 
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -18,7 +19,7 @@ export default function App() {
         translucent
       />
 
-     { fontsLoaded ? <SignIn/> : <Loading />}
+     { fontsLoaded ? <SignUp/> : <Loading />}
     </GluestackUIProvider>
   );
 }
---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,61 @@
+import { VStack, ImageBackground, Text, Center, Heading, ScrollView, SafeAreaView } from '@gluestack-ui/themed'
+
+import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from "@assets/background.png"
+
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+export function SignUp() {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#1F2937' }}>
+            <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false} bounces={false}>
+                <VStack flex={1} bg="$trueGray800" pb={64}>
+                    <ImageBackground 
+                        source={BackgroundImg}
+                        alt="Pessoas treinando"
+                        style={{ width: '100%', height: '100%' }}
+                        resizeMode='stretch'
+                        position='absolute'
+                        />
+
+                    <Center my={96}>
+                        <LogoSvg />
+
+                        <Text color="$mygray100" fontSize="$sm">
+                            Treine sua mente e o seu corpo    
+                        </Text>        
+                    </Center>
+
+                    <Center px={28}>
+                        <Heading color="$mygray100" fontSize="$xl" mb={24} fontFamily="$heading">
+                            Crie sua conta
+                        </Heading>
+
+                        <Input 
+                            message="Nome"
+                        />
+                            
+                        <Input 
+                            message="E-mail"
+                        />
+                        <Input 
+                            message="Senha"
+                        />
+
+                        <Button 
+                            title="Criar e acessar"
+                            />
+                    </Center>
+
+                    <Center px={28} mt={96}>
+                        <Button 
+                            title="Voltar para o login"
+                            variant='outline'
+                        />
+                    </Center>
+                </VStack>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
