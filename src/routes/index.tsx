62586de0018ae0fc663f08129336e676b8482635
Ap274 <aldopiresaps@gmail.com>
2024-05-22 15:58:34 -0300

    feat: type app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { AuthRoutes } from "./auth.routes";
 import { Box } from "@gluestack-ui/themed";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
     return (
         <Box flex={1} bg="$trueGray800">
             <NavigationContainer>
-                <AuthRoutes />
+                <AppRoutes />
             </NavigationContainer>
         </Box>
     )
