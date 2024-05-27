52fbbaaf8086270d43f52e944c54c91b7b13ab1e
Ap274 <aldopiresaps@gmail.com>
2024-05-27 19:43:45 -0300

    feat: finishing private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import { Box } from "@gluestack-ui/themed";
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { config } from '../../config/gluestack-ui.config';
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
+    const { colors } = config.tokens;
+
+    const theme = DefaultTheme;
+    theme.colors.background = colors.mygray700
+
     return (
         <Box flex={1} bg="$trueGray800">
-            <NavigationContainer>
+            <NavigationContainer theme={theme}>
                 <AppRoutes />
             </NavigationContainer>
         </Box>
